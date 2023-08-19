30a72a6b7773e7233585e7e5ac44278e8a3648c2
jcharra <johannes.charra@gmail.com>
2023-08-19 21:18:57 +0200

    limit to 50 words, add results

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # frenchnouns
 
 Train your expertise in one of the most difficult tasks ever: Finding the proper article for a French noun.
+Check it out at https://frenchnouns.vercel.app
 
 ### Running locally:
 
@@ -15,5 +16,7 @@ Then run the local dev server with
 ```
 npm run dev
 ```
+
+and visit the app at http://localhost:3000
 
 Bon courage!
--- public/script.js
+++ public/script.js
@@ -3,13 +3,30 @@ let currentGender = "";
 let count = 0;
 let correct = 0;
 let starttime = 0;
+const COUNT_LIMIT = 50;
+
+function reset_values() {
+  current = "";
+  currentGender = "";
+  count = 0;
+  correct = 0;
+  starttime = 0;
+
+  const table = document.getElementById("history_table");
+  while (table.rows.length > 0) {
+    table.deleteRow(0);
+  }
+
+  document.getElementById("stats").innerHTML = "";
+  document.getElementById("stats_perc").innerHTML = "";
+}
 
 async function next() {
   const res = await fetch("/api/noun");
   return await res.json();
 }
 
-async function show_next() {
+async function display_next_word() {
   const data = await next();
   starttime = new Date();
 
@@ -19,7 +36,7 @@ async function show_next() {
     document.getElementById("word").innerHTML = current;
   } else {
     console.error("Error fetching noun");
-    setTimeout(200, show_next);
+    setTimeout(200, display_next_word);
   }
 }
 
@@ -33,9 +50,28 @@ function evaluate_answer(gender) {
   push_history(current, isCorrect, time_in_secs);
   count++;
 
+  refresh_stats();
+
+  if (count < COUNT_LIMIT) {
+    display_next_word();
+  } else {
+    display_result();
+  }
+}
+
+function refresh_stats() {
   document.getElementById("stats").innerHTML = correct + " / " + count;
   document.getElementById("stats_perc").innerHTML = ((100 * correct) / count).toFixed(2) + "%";
-  show_next();
+}
+
+function display_result() {
+  //document.getElementById("points").innerHTML = correct;
+  const prec_squared = ((correct / count) ** 2).toFixed(2);
+  //document.getElementById("prec_squared").innerHTML = prec_squared;
+  const total = Math.round(prec_squared * correct);
+  document.getElementById("total").innerHTML = total;
+  show_section("result");
+  reset_values();
 }
 
 function push_history(word, correct, time) {
@@ -51,6 +87,25 @@ function push_history(word, correct, time) {
   word_cell.innerHTML = word;
   eval_cell.innerHTML = correct ? "✅" : "❌";
   time_cell.innerHTML = time + "s";
+
+  word_cell.classList.add("wide_cell");
 }
 
-show_next();
+function show_section(section_id) {
+  const intro_elem = document.getElementById("intro");
+  const game_elem = document.getElementById("game");
+  const result_elem = document.getElementById("result");
+
+  for (const elem of [intro_elem, game_elem, result_elem]) {
+    if (elem.id === section_id) {
+      elem.classList.remove("hidden");
+    } else {
+      elem.classList.add("hidden");
+    }
+  }
+}
+
+function start() {
+  show_section("game");
+  display_next_word();
+}
